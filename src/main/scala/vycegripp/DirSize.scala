b870dfa72596229254b87834280c2c792cb85601
Cary Scofield <carys689@gmail.com>
2014-06-02 09:49:23 -0400

    Correct version of DirSize tool

--- src/main/scala/vycegripp/DirSize.scala
+++ src/main/scala/vycegripp/DirSize.scala
@@ -1,101 +1,85 @@
-import java.io.File;
-import scala.collection.mutable.ArrayBuffer;
+package vycegripp
 
+import java.io.File
+
+/**
+ * This program will traverse a directory tree at a given starting point and produce
+ * a listing of files and directories along with their sizes.
+ */
 object DirSize {
 
-    var SHOW_FILES : Boolean = false;
-    var DEBUGGING : Boolean = false;
-    var FILE_TYPES : Array[String]=Array.fill(10)("*");
-    var ALL_FILES : Boolean = true;
-   
+    var SHOW_FILES : Boolean = false
+    var numFiles : Long = 0
+    var numDirectories : Long = 0
+    var startTime : Long = 0
 
-    def subdirs(dir: File): Iterator[File] = {
-        val children : Array[File] = dir.listFiles.filter(_.isDirectory);
-        children.toIterator ++ children.toIterator.flatMap(subdirs _);
-    }
-
-    def wantedFileType( path : String ) : Boolean = {
-        if( ALL_FILES ) true;
-        else {
-            for( i <- 0 until FILE_TYPES.length ) {
-                if( FILE_TYPES(i) != "*" ) {
-                    return path.endsWith( FILE_TYPES( i ) );
-                }
-            }
-        }
-        false
-    }
-    
-    def dirfiles(dir: File): Iterator[File] = {
-        //println( "\tdir=" + dir );
-        var children : Array[File] = dir.listFiles() filter( x => wantedFileType( x.getPath() ) );
-        if( children == null ) children = new Array[File](0);
-        children.toIterator;
-    }
-
-    // def listfiles( files: Iterator[File] ) : String = {
-    //     var filelist : StringBuilder = new StringBuilder();
-    //     for( f <- files ) filelist.append( f ).append( "," );
-    //     filelist.toString();
-    // }
-
-    def getAttributes( f: File ) : String = {
-        var attribs : StringBuilder = new StringBuilder();
-        if( f.isDirectory() ) attribs.append( "directory " );
-        else if( f.isFile() ) attribs.append( "file " );
-        else if( f.isHidden() ) attribs.append( "hidden " );
-        else if( f.getName().endsWith( ".lnk" ) ) attribs.append( "link " );
-        if( !f.isAbsolute() ) attribs.append( "absolute=no " );
-        attribs.append( "length=" + f.length() );
-        attribs.toString();
-    }
-
-    def getSize( f: File ) : Long = {
-        if( DEBUGGING ) println( "\tDEBUG: " + f.getPath() + " " + getAttributes( f ) );
-        var size : Long = 0;
-        var dirsize : Long = 0;
-        if( f.isDirectory() ) {
-            var files = new ArrayBuffer[File]();
-            for( d <- dirfiles( f ) ) {
-                size = getSize( d );
-                dirsize += size;
-                //println( d.getPath() + " has " + size + " bytes" );
-                if( SHOW_FILES && !d.isDirectory() ) println( "\t" + size + ": " + d.getPath() );
-            }
-            println( dirsize + ": " + f.getPath() + "\\" );
-        }
-        else if( f.getPath().endsWith( ".lnk" ) ) {
-            //println( "\tLINK: " + f.getPath() );
-            // ignore
-        }
-        else {
-
-            dirsize = f.length();
-            // println( f.getPath() + " has " + size + " bytes" );
-        }
-        dirsize;
-    }
-
+    /**
+     * Up to two arguments can be passed: 1) if '--files' given, print the names/sizes of the files;
+     * otherwise just print the names/sizes of the directories; 2) the root directory's name for
+     * directory traversal starting point.
+     */
     def main( args: Array[String]) {
-        var rootdir : String = "";
-        var files = new ArrayBuffer[File]();
-        var fileTypeArgs : Boolean = false;
+        var rootdir : String = ""
+        var fileTypeArgs : Boolean = false
         for( i <- 0 to args.length-1 ) {
-            if( args(i) == "--files" ) SHOW_FILES = true;
-            else if( args(i) == "--debug" ) DEBUGGING = true;
-            else if( args(i) == "--types" ) fileTypeArgs = true;
-            else if( fileTypeArgs ) { FILE_TYPES = args(i).split('|'); ALL_FILES = false; }
-            else rootdir = args(i);
+            if( args(i) == "--files" ) SHOW_FILES = true
+            else rootdir = args(i)
         }
-        // for( d <- dirfiles(new File(rootdir))) files += d;
-        // //println( files );
-        // var size: Long = 0;
-        // var f: File = null;
-        // for( f <- files ) size += getSize( f );
 
-        var size : Long = 0;
-        size = getSize( new File(rootdir) );
+        var size : Long = 0
+        startTime = System.currentTimeMillis
+        size = getSize( new File(rootdir) )
 
-        println( size + ": " + rootdir.replace( '/', '\\' ) + "\\" );
+        println( "# files=%d # directories=%d".format( numFiles, numDirectories ) )
+        println( "Total time=%f seconds".format( (System.currentTimeMillis() - startTime ).asInstanceOf[Double]/1000.0D ) )
     }
+
+    /**
+     * Get the elements of a directory.
+     */
+    def dirfiles(dir: File): Iterator[File] = {
+        var children : Array[File] = dir.listFiles()
+        if( children == null ) children = new Array[File](0)
+        children.toIterator
+    }
+
+    /**
+     * Get the attributes of a file (for debugging purposes).
+     */
+    def getAttributes( f: File ) : String = {
+        var attribs : StringBuilder = new StringBuilder()
+        if( f.isDirectory() ) attribs.append( "directory " )
+        else if( f.isFile() ) attribs.append( "file " )
+        else if( f.isHidden() ) attribs.append( "hidden " )
+        else if( f.getName().endsWith( ".lnk" ) ) attribs.append( "link " )
+        if( !f.isAbsolute() ) attribs.append( "absolute=no " )
+        attribs.append( "length=" + f.length() )
+        attribs.append( " name=" + f.getName() )
+        attribs.toString()
+    }
+
+    /**
+     * Depth-first recursive traversal of the directory tree printing names and sizes of the
+     * elements (i.e., directories and files) and accumulating the sizes of the directories.
+     */
+    def getSize( f: File ) : Long = {
+        //println( getAttributes( f ) )
+        var size : Long = 0
+        var dirsize : Long = 0
+        if( f.isDirectory() ) {
+            for( d <- dirfiles( f ) ) {
+                size = getSize( d )
+                dirsize += size
+                if( !d.isDirectory() ) numFiles += 1
+                if( SHOW_FILES && !d.isDirectory() ) println( " f %9d %s".format( size, d.getPath() ) )
+            }
+            numDirectories += 1
+            println( "d %10d %s".format( dirsize, f.getPath() ) )
+        }
+        else {
+            dirsize = f.length()
+        }
+        dirsize
+    }
+
 }
