c3b04bea6e88608691b85895140ae7d4d5871474
Cary Scofield <carys689@gmail.com>
2014-10-05 10:41:45 -0400

    Streamline assertion code

--- src/main/scala/ods/StackTest.scala
+++ src/main/scala/ods/StackTest.scala
@@ -8,6 +8,12 @@ package ods
 
 object StackTest {
 
+  def assertTrue( expression : Boolean, msgIfNotTrue : String = "Not OK!" ) : Boolean = {
+    if( expression ) println( "OK!" )
+    else println( s"Assertion failure!: $msgIfNotTrue" )
+    expression
+  }
+
   def main(args: Array[String]): Unit = {
 
     var s = new Stack[String]
@@ -15,36 +21,36 @@ object StackTest {
     s.push( "2nd String" )
     s.push( "Another String" )
     println( "s=" + s )
-    if( s.peek == "Another String" ) println( "OK!" ); else println( "Not OK!" )
+    assertTrue( s.peek == "Another String",  """s.peek not equal to "Another String" """ )
     println( s.pop )
-    if( s.peek == "2nd String" ) println( "OK!" ); else println( "Not OK!" )
+    assertTrue( s.peek == "2nd String", """s.peek not equal to "2nd String" """ )
     println( s.pop )
-    if( s.peek == "A String" ) println( "OK!" ); else println( "Not OK!" )
+    assertTrue( s.peek == "A String", """s.peek not equal to "A String" """ )
     println( s.pop )
-    if( s.peek == null ) println( "OK!" ); else println( "Not OK!" )
+    assertTrue( s.peek == null, """s.peek not equal to null""" )
 
     var s2 = new Stack[Any]
     s2.push( List(1,1,2,3,5,8))
-    if( s2.peek == List(1,1,2,3,5,8) ) println( "OK!" ); else println( "Not OK!" )
+    assertTrue( s2.peek == List(1,1,2,3,5,8) )
     s2.push( Set(1,1,2,3,4) )
-    if( s2.peek == Set(4,3,2,1) ) println( "OK!" ); else println( "Not OK!" )
+    assertTrue( s2.peek == Set(4,3,2,1) )
     s2.push( 3.14159 )
     s2.push( new java.math.BigInteger( "258000000000000000" ) )
-    if( s2.contains( 3.14159) ) println( "OK!" ); else println( "Not OK!" )
+    assertTrue( s2.contains( 3.14159) )
     println( "s2=" + s2 )
     val s3 = s2.cloneIt
     println( "s3=" + s3 )
-    if( s2 == s3 ) println( "OK!" ); else println( "Not OK!" )
+    assertTrue( s2 == s3 )
     s2.pop()
-    if( s2 != s3 ) println( "OK!" ); else println( "Not OK!" )
-    if( s2 != null ) println( "OK!" ); else println( "Not OK!" )
+    assertTrue( s2 != s3 )
+    assertTrue( s2 != null )
     s2.clear()
-    if( s2.peek == null ) println( "OK!" ); else println( "Not OK!" )
-    if( s2.size == 0 ) println( "OK!" ); else println( "Not OK!" )
-    if( s2.isEmpty )  println( "OK!" ); else println( "Not OK!" )
-    if( s3.size > 0 ) println( "OK!" ); else println( "Not OK!" )
-    if( s3.peek.asInstanceOf[java.math.BigInteger].doubleValue() == 2.58E17 )
-        println( "OK!" ); else println( "Not OK!" )
+    assertTrue( s2.peek == null )
+    assertTrue( s2.size == 0 )
+    assertTrue( s2.isEmpty )
+    assertTrue( s3.size > 0 )
+    assertTrue( s3.peek.asInstanceOf[java.math.BigInteger].doubleValue() == 2.58E17 )
+
 
   }
 
